b4a9f97c339974e6cdc65edd99bc0a2dc16f1d99
Gabs-jg <gabriel19joao@gmail.com>
2026-07-24 18:25:01 -0300

    Inserindo personagem na tela e fazendo ele movimentar

--- src/jogo/Cenario1.java
+++ src/jogo/Cenario1.java
@@ -8,18 +8,21 @@ public class Cenario1 {
 
     private Window janela;
     private Scene cenario; // Isso define o arquivo que molda o cenário. Sempre usar essas classes em classes que formam o cenário
+    private Jogador jogador;
 
     public Cenario1(Window janela) {
         this.janela = janela; // Pega o mesmo tamanho da janela da classe main
         cenario = new Scene(); // Instancia a cena
         cenario.loadFromFile(URL.scenario("Cenario1.txt")); // Carrega o arquivo de cenário
-
+        jogador = new Jogador(640, 350); // Instancia o jogador em tais cooredenadas
         run(); // Loop infinito, o mesmo da classe main
     }
 
     private void run() {
         while(true) {
             cenario.draw();
+            jogador.draw();
+            jogador.mover();
             janela.update();
         }
     }
