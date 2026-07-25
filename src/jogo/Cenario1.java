3de409d99073d7285b0c030c9692a5247ba589a9
Gabs-jg <gabriel19joao@gmail.com>
2026-07-25 17:47:10 -0300

    Colocando inimigo (zumbi) no cenário e fazendo ele perseguir o personagem

--- src/jogo/Cenario1.java
+++ src/jogo/Cenario1.java
@@ -11,6 +11,7 @@ public class Cenario1 {
     private Scene cenario; // Isso define o arquivo que molda o cenário. Sempre usar essas classes em classes que formam o cenário
     private Jogador jogador;
     private Keyboard teclado;
+    private Zumbi zumbi;
 
     public Cenario1(Window janela) {
         this.janela = janela; // Pega o mesmo tamanho da janela da classe main
@@ -18,6 +19,8 @@ public class Cenario1 {
         cenario.loadFromFile(URL.scenario("Cenario1.txt")); // Carrega o arquivo de cenário
         jogador = new Jogador(640, 350); // Instancia o jogador em tais cooredenadas
         teclado = janela.getKeyboard();
+        zumbi = new Zumbi(300, 300);
+
         Som.play("musica.wav");
         run(); // Loop infinito, o mesmo da classe main
     }
@@ -27,13 +30,19 @@ public class Cenario1 {
             //cenario.draw();
             jogador.controle(janela, teclado);
             jogador.caminho(cenario);
+            zumbi.caminho(cenario);
+            zumbi.perseguir(jogador.x, jogador.y);
 
             cenario.moveScene(jogador);
 
             jogador.x += cenario.getXOffset();
             jogador.y += cenario.getYOffset();
 
+            zumbi.x += cenario.getXOffset();
+            zumbi.y += cenario.getYOffset();
+
             jogador.draw();
+            zumbi.draw();
             janela.update();
 
         }
---
+++ src/jogo/Zumbi.java
@@ -0,0 +1,60 @@
+package jogo;
+
+import jplay.URL;
+
+public class Zumbi extends Ator {
+
+
+
+    public Zumbi(int x, int y) {
+        super(URL.sprite("Zumbi0.png"), 16);
+        this.x = x;
+        this.y = y;
+        this.setTotalDuration(2000);
+        this.velocidade = 0.03;
+    }
+
+    public void perseguir(double x, double y) {
+        if(this.x > x && this.y <= y + 50 && this.y >= y - 50) { // pra direita
+            moveTo(x, y, velocidade);
+            if(direcao != 1) {
+                setSequence(4, 8);
+                direcao = 1;
+            }
+            movendo = true;
+        }
+
+        else if(this.x < x && this.y <= y + 50 && this.y >= - 50) { // pra esquerda
+            moveTo(x, y, velocidade);
+            if(direcao != 2) {
+                setSequence(9, 12);
+                direcao = 2;
+            }
+            movendo = true;
+        }
+
+        else if(this.y > y) { // pra cima
+            moveTo(x, y, velocidade);
+            if(direcao != 4) {
+                setSequence(12, 16);
+                direcao = 4;
+            }
+            movendo = true;
+        }
+
+        else if(this.y < y) { // pra baixo
+            moveTo(x, y, velocidade);
+            if(direcao != 3) {
+                setSequence(0, 4);
+                direcao = 3;
+            }
+            movendo = true;
+        }
+
+        if(movendo) {
+            update();
+            movendo = false;
+        }
+    }
+
+}
